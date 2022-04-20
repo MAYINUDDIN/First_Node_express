14e43dfe0f5985a6ab7786eabb0f93a6a524eb45
MAYINUDDIN <mainu@gmail.com>
2022-04-20 22:10:49 +0600

    first commit

---
+++ steps.js
@@ -0,0 +1,12 @@
+//create a folder\
+//open commad line to that folder\
+//npm init
+//npm install express
+//create index.js
+//--------
+//for express/node server
+// 1.Node Express
+// 2.Declare prompt
+// 3.liten toport
+// 4. node app.js
+//-----------
